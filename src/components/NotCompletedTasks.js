baa7a1b18e226f2e8cd9136190c9902dff1f02e5
Suma249 <3584_suma@katalystindia.org>
2024-12-10 12:31:28 +0530

    trying to implemnet bulsk sleection and deselction feature

--- src/components/NotCompletedTasks.js
+++ src/components/NotCompletedTasks.js
@@ -7,7 +7,20 @@ function NotCompletedTasks({ task }) {
     return (
         <div>
             <li>
-                <input type="checkbox" checked={false} onChange={() => { if (window.confirm("are you sure you want to mark the task" + task.task + "as completed?, once you mark it you wont be able to unmark it")) changeTaskStatus(task.id) }} /> {task.task} <button onClick={() => { if (window.confirm("are you sure you want to delete this task?")) deleteTask(task.id) }} >x</button>
+                <input
+                    type="checkbox"
+                    checked={false}
+                    onChange={
+                        () => {
+                            if (window.confirm("mark this task as completed?"))
+                                changeTaskStatus(task.id)
+                        }}
+                />
+                {task.task}
+                <button
+                    onClick={() => { if (window.confirm("are you sure you want to delete this task?")) deleteTask(task.id) }} >
+                    x
+                </button>
             </li>
         </div>
     )
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,11 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { GloablContext } from '../context/global'
 import CompletedTasks from './CompletedTasks'
 import NotCompletedTasks from './NotCompletedTasks'
 
 export default function Tasks() {
     const { tasks } = useContext(GloablContext)
+    const [selectAll, setSelectAll] = useState(false)
     //console.log(tasks)
     console.log("re rendering tasks component as there was change in parent's state")
     if (tasks === null || tasks.length === 0)
@@ -14,7 +15,10 @@ export default function Tasks() {
     return (
 
         < div >
-            <p className='completedTasks'>Completed Tasks</p>
+            <p className='completedTasks' style={{ display: 'inline-block' }}>
+                <input type='checkbox' checked={selectAll} />
+                <h3 style={{ display: 'inline' }} >Completed Tasks</h3>
+            </p>
             <ul>
                 {
                     completedTasks.map(task => <CompletedTasks key={task.id} task={task} />)
